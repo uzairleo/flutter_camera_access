6e10bd0cbed7cc8a347a726adb54ca4c19c5f524
Uzair Leo <uzair.jan336@gmail.com>
2020-05-20 05:36:38 +0500

    add front and rear logic

--- lib/TakingCameraImage.dart
+++ lib/TakingCameraImage.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter_camera_access/DisplayCameraImage.dart';
+import 'package:flutter_camera_access/main.dart';
 import 'package:path/path.dart' show join;
 import 'package:path_provider/path_provider.dart';
 
-
 class TakingCameraImage extends StatefulWidget {
-final CameraDescription camera;
-  TakingCameraImage({Key key,@required this.camera}) : super(key: key);
+  final CameraDescription camera;
+  final cameras;
+  TakingCameraImage({Key key, this.cameras, @required this.camera})
+      : super(key: key);
 
   @override
   _TakingCameraImageState createState() => _TakingCameraImageState();
@@ -16,75 +18,97 @@ final CameraDescription camera;
 class _TakingCameraImageState extends State<TakingCameraImage> {
   CameraController _controller;
   Future<void> _initializedFutureController;
-
-@override
+  int cameraIndex = 0;
+  @override
   void initState() {
     super.initState();
- 
-    _controller=CameraController(
-      widget.camera,
-       ResolutionPreset.medium
-       ,);
 
-    _initializedFutureController=_controller.initialize();
- 
+initializedcamera(cameraIndex);
   }
+  initializedcamera(int cameraIndex){
+    int indexcamera=cameraIndex;
+    _controller = CameraController(
+      // widget.camera,
+      cameras[indexcamera],
+      // widget.cameras[2],
+      ResolutionPreset.medium,
+    );
+
+    _initializedFutureController = _controller.initialize();
+  }
+
   @override
   void dispose() {
     _controller.dispose();
     super.dispose();
   }
-  
+var cameraStatus=Icons.camera_rear;
   @override
   Widget build(BuildContext context) {
 //   final size = MediaQuery.of(context).size;
 // final deviceRatio = size.width / size.height;
     return Scaffold(
       appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(cameraStatus), 
+            onPressed: () async{
+              setState(() {
+                (cameraStatus==Icons.camera_rear)
+                ?cameraStatus=Icons.camera_front
+                :cameraStatus=Icons.camera_rear;
+              });
+              (cameraIndex==0)?cameraIndex=1:cameraIndex=0;
+              initializedcamera(cameraIndex);
+            })
+        ],
         title: Text("Leo Camera"),
         centerTitle: true,
-        ),
-        body: FutureBuilder<void>(
+      ),
+      body: FutureBuilder<void>(
           future: _initializedFutureController,
-          builder: (context,snapshot){
-            if(snapshot.connectionState==ConnectionState.done){
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
               return CameraPreview(_controller);
-          //    return  Transform.scale(
-          // scale: _controller.value.aspectRatio / deviceRatio,
-          // child: Center(
-          //   child: AspectRatio(
-          //     aspectRatio: _controller.value.aspectRatio,
-          //     child: CameraPreview(_controller), //cameraPreview
-          //   ),
-          // ));
-            }else{
-              return Center(child: CircularProgressIndicator(),);
+              //    return  Transform.scale(
+              // scale: _controller.value.aspectRatio / deviceRatio,
+              // child: Center(
+              //   child: AspectRatio(
+              //     aspectRatio: _controller.value.aspectRatio,
+              //     child: CameraPreview(_controller), //cameraPreview
+              //   ),
+              // ));
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
             }
           }),
-          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.camera),
-            onPressed: ()async{
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.camera),
+          onPressed: () async {
+            try {
+              await _initializedFutureController;
 
-                try{
-                    await _initializedFutureController;
+              final path = join(
+                (await getTemporaryDirectory()).path,
+                '${DateTime.now()}.png',
+              );
+              await _controller.takePicture(path);
 
-                    final path=join(
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          (DisplayCameraImage(imagePath: path))));
 
-                    (await getTemporaryDirectory()).path,
-                    '${DateTime.now()}.png',);
-                    await _controller.takePicture(path);
-
-
-                    Navigator.push(context,MaterialPageRoute(
-                      builder: (context)=>(DisplayCameraImage(imagePath:path))
-                    ));
-                    
-                  print("done");
-                }catch(e){
-                  print("$e******************Sorry bro some Error happened there sorry bro again *****************");
-                }
-            }),
+              print("done");
+            } catch (e) {
+              print(
+                  "$e******************Sorry bro some Error happened there sorry bro again *****************");
+            }
+          }),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import './TakingCameraImage.dart';
-
+List<CameraDescription> cameras;
 Future<void> main ()async{
 
 //ensure camera initialization  
 WidgetsFlutterBinding.ensureInitialized();
 //now check for the available list of cameras
-final cameras= await availableCameras();
+ cameras= await availableCameras();
+
 final firstCamera=  cameras.first;
 
-  runApp(new MyApp(firstCamera:firstCamera));
+
+  runApp(new MyApp(firstCamera:firstCamera,camerasList: cameras,));
 }
 
 class MyApp extends StatelessWidget {
+  final camerasList;
   final CameraDescription firstCamera;
-  MyApp({this.firstCamera});
+  MyApp({this.firstCamera,this.camerasList});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
     home: TakingCameraImage(
+      cameras: camerasList,
       camera: firstCamera,
     ),
   );
